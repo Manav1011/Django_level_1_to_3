2ab5dff4b6083762a2aef4b277303ad21eadb362
Manav <manavshah1011.ms@gmail.com>
2022-06-07 20:20:50 +0530

    models commit

--- crudapp/admin.py
+++ crudapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import name,age,gender
 
 # Register your models here.
+admin.site.register(name)
+admin.site.register(age)
+admin.site.register(gender)
